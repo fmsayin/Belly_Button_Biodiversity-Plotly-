3d03e77c15d4a7f2d5b1d960db019d6ab6989206
FMSUSA <fmsayin@hotmail.com>
2019-02-27 20:59:44 -0600

    update

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -28,82 +28,83 @@ function buildMetadata(sample) {
 
 
 function buildCharts(sample) {
-    
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
-  
-    // console.log(sample)  
-  d3.json(`/samples/${sample}`).then(function (sampleData) {
-    console.log(sampleData);
-    
-    const otu_ids = sampleData.otu_ids;
-    const otu_labels = sampleData.otu_labels;
-    const sample_values = sampleData.sample_values;
 
-    // @TODO: Build a Bubble Chart using the sample data
-    var bubbleData = [{
-      x: otu_ids,
-      y: sample_values,
-      text: otu_labels,
-      mode: 'markers',
-      marker: {
-        size: sample_values,
-        color: otu_ids,
-        colorscale: 'Earth'
-      }
-    }];      
+      // @TODO: Use `d3.json` to fetch the sample data for the plots
+      var url = `/samples/${sample}`;
+      d3.json(url).then((data) => {
 
-    var bubbleLayout = {
-      margin: { t: 0 },
-      hovermode: 'closest',
-      xaxis: {title: 'OTU ID'},
-    };
+        // @TODO: Build a Bubble Chart using the sample data
+        var x_values = data.otu_ids;
+        var y_values = data.sample_values;
+        var m_size = data.sample_values;
+        var m_colors = data.otu_ids;
+        var t_values = data.otu_labels;
 
-    Plotly.plot('bubble', bubbleData, bubbleLayout);
+        var trace1 = {
+          x: x_values,
+          y: y_values,
+          text: t_values,
+          mode: 'markers',
+          marker: {
+            color: m_colors,
+            size: m_size
+          }
+        };
 
-    // @TODO: Build a Pie Chart
-    d3.json(url).then(function(data) {  
-      var pie_values = data.sample_values.slice(0,10);
-        var pie_labels = data.otu_ids.slice(0,10);
-        var pie_hover = data.otu_labels.slice(0,10);
-  
-        var data = [{
-          values: pie_values,
-          labels: pie_labels,
-          hovertext: pie_hover,
-          type: 'pie'
-        }];
-  
-        Plotly.newPlot('pie', data);
-  
+        var data = [trace1];
+
+        var layout = {
+          xaxis: { title: "OTU ID" },
+        };
+
+        Plotly.newPlot('bubble', data, layout);
+
+
+        // @TODO: Build a Pie Chart
+        d3.json(url).then(function (data) {
+          var pie_values = data.sample_values.slice(0, 10);
+          var pie_labels = data.otu_ids.slice(0, 10);
+          var pie_hover = data.otu_labels.slice(0, 10);
+
+          var data = [{
+            values: pie_values,
+            labels: pie_labels,
+            hovertext: pie_hover,
+            type: 'pie'
+          }];
+
+          Plotly.newPlot('pie', data);
+
+        });
       });
-    });   
-  }
+    }
+
 
 function init() {
-// Grab a reference to the dropdown select element
-var selector = d3.select("#selDataset");
+      // Grab a reference to the dropdown select element
+      var selector = d3.select("#selDataset");
 
-// Use the list of sample names to populate the select options
-d3.json("/names").then((sampleNames) => {
-  sampleNames.forEach((sample) => {
-    selector
-      .append("option")
-      .text(sample)
-      .property("value", sample);
-  });
+      // Use the list of sample names to populate the select options
+      d3.json("/names").then((sampleNames) => {
+        sampleNames.forEach((sample) => {
+          selector
+            .append("option")
+            .text(sample)
+            .property("value", sample);
+        });
 
-  // Use the first sample from the list to build the initial plots
-  const firstSample = sampleNames[0];
-  buildCharts(firstSample);
-  buildMetadata(firstSample);
-});
-}
+        // Use the first sample from the list to build the initial plots
+        const firstSample = sampleNames[0];
+        buildCharts(firstSample);
+        buildMetadata(firstSample);
+      });
+    }
 
 function optionChanged(newSample) {
-// Fetch new data each time a new sample is selected
-buildCharts(newSample);
-buildMetadata(newSample);
-}
+      // Fetch new data each time a new sample is selected
+      buildCharts(newSample);
+      buildMetadata(newSample);
+    }
 
 // Initialize the dashboard
 init();
